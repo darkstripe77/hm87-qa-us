24eb36a4d9e0a704edc3d0c6ee11af1e06fb7cab
Nicholas Espinoza <aesp.nick@gmail.com>
2024-08-29 15:37:39 -0500

    Completed all tasks: Implemented GET, POST, PUT, DELETE tests and added README.md with project details

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-fa9cdf54-5115-4e5d-917f-f7d7c979ebe0.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-7bf56f24-82b3-46e3-b9cc-ca55e42f7e47.containerhub.tripleten-services.com"
 }
